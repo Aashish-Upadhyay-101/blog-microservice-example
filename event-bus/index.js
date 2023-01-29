41a39693cc3df0fe7057c9808320eb08db513662
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-01-30 00:55:55 +0545

    basic event-bus

---
+++ event-bus/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:4000/events", event); // post server
+  axios.post("http://localhost:4001/events", event); // comment server
+  axios.post("http://localhost:4002/events", event); // queryService server
+
+  res.send({ status: "OK" });
+});
+
+app.listen(4003, () => {
+  console.log("Server running on http://localhost:4002");
+});
