3cc061d1249168092e94d46c0b0126500c77b08e
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-02-06 09:00:20 +0545

    comment moderation

--- comments/index.js
+++ comments/index.js
@@ -40,8 +40,28 @@ app.post("/posts/:id/comments", async (req, res) => {
     res.status(201).send(commentsByPostId[id]);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
     console.log("Event Received", req.body.type);
+
+    const { type, data } = req.body;
+
+    if (type === "CommentModerated") {
+        const { postId, id, status, content } = data;
+
+        const comments = commentsByPostId[postId];
+        const comment = comments.find((comment) => comment.id === id);
+        comment.status = status;
+
+        axios.post("http://localhost:4003/events", {
+            type: "CommentUpdated",
+            data: {
+                id,
+                postId,
+                content,
+                status,
+            },
+        });
+    }
     res.send({});
 });
 
